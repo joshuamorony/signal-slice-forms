c133fe839e27ef2e047d1c69f8be6cc4107e54f1
Joshua Morony <joshua.morony@gmail.com>
2023-12-12 12:11:29 +1030

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,11 +6,7 @@ import { RouterOutlet } from '@angular/router';
   selector: 'app-root',
   standalone: true,
   imports: [CommonModule, RouterOutlet],
-  template: `
-    <h1>Welcome to {{title}}!</h1>
-
-    <router-outlet></router-outlet>
-  `,
+  template: ` <router-outlet></router-outlet> `,
   styles: [],
 })
 export class AppComponent {
---
+++ src/app/home/home.component.ts
@@ -0,0 +1,103 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, effect, inject } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { signalSlice } from 'ngxtension/signal-slice';
+import { Observable, of } from 'rxjs';
+import {
+  catchError,
+  delay,
+  map,
+  startWith,
+  switchMap,
+  tap,
+} from 'rxjs/operators';
+
+@Component({
+  standalone: true,
+  selector: 'app-home',
+  template: `
+    <form [formGroup]="form" (ngSubmit)="formState.submit()">
+      <input type="text" formControlName="name" />
+      <select formControlName="likesSignals">
+        <option value="">Select the correct option</option>
+        <option value="yes">Yes</option>
+        <option value="no">No</option>
+      </select>
+      <textarea
+        formControlName="explanation"
+        placeholder="explain yourself..."
+      ></textarea>
+      <button type="submit">Submit</button>
+    </form>
+  `,
+  imports: [ReactiveFormsModule],
+})
+export default class HomeComponent {
+  private fb = inject(FormBuilder);
+  private http = inject(HttpClient);
+
+  formDataLoaded$ = of({ name: 'Josh' }).pipe(delay(2000));
+
+  form = this.fb.nonNullable.group({
+    name: [''],
+    likesSignals: ['yes'],
+    explanation: [{ value: '', disabled: true }],
+  });
+
+  initialFormState = {
+    ...this.form.getRawValue(),
+    status: 'loading' as
+      | 'loading'
+      | 'loaded'
+      | 'submitting'
+      | 'success'
+      | 'error',
+  };
+
+  formState = signalSlice({
+    initialState: this.initialFormState,
+    sources: [
+      this.form.valueChanges.pipe(map(() => this.form.getRawValue())),
+      this.formDataLoaded$.pipe(
+        tap((data) => this.form.patchValue(data)),
+        map(() => ({ status: 'loaded' as const }))
+      ),
+    ],
+    actionSources: {
+      submit: (_, action$: Observable<void>) =>
+        action$.pipe(
+          switchMap(() =>
+            this.http.post('someapi', this.form.getRawValue()).pipe(
+              map(() => ({ status: 'success' as const })),
+              catchError(() => of({ status: 'error' as const })),
+              startWith({ status: 'submitting' as const })
+            )
+          )
+        ),
+    },
+    effects: (state) => ({
+      init: () => {
+        this.form.disable({ emitEvent: false });
+      },
+      enableAfterLoad: () => {
+        const status = state.status();
+        if (status === 'loaded') {
+          this.form.enable({ emitEvent: false });
+        }
+      },
+      requireExplanation: () => {
+        const likesSignals = state.likesSignals();
+        if (likesSignals === 'no') {
+          this.form.controls.explanation.enable({ emitEvent: false });
+          this.form.controls.explanation.setValidators(Validators.required);
+        }
+      },
+    }),
+  });
+
+  constructor() {
+    effect(() => {
+      console.log(this.formState());
+    });
+  }
+}
